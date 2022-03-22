6e896960f995d471a6fdfb1f51d60af35b4317c4
eduardomoscardinisouza <eduardo.fca@gmail.com>
2022-03-22 20:09:50 -0300

    Commit Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,15 +2,51 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
 
-        Gato gato = new Gato ();
-
+        Gato gato = new Gato();
         System.out.println(gato);
 
-        int a = 3;
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
+        /*int a = 3;
         int b = 5;
-        System.out.println("Hello World! " + (a+b));
-           }
+        System.out.println("Hello World! " + (a+b));*/
+    }
 }
+    class Livro{
+        private String nome;
+        private Integer NumPaginas;
+
+        public Livro(String nome, Integer numPaginas) {
+            this.nome = nome;
+            NumPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPaginas() {
+            return NumPaginas;
+        }
+
+        public void setNumPaginas(Integer numPaginas) {
+            NumPaginas = numPaginas;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", NumPaginas=" + NumPaginas +
+                    '}';
+        }
+    }
